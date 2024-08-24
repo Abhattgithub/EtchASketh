8e639ff7005b54dea80ebd5995b89d6933bbdd3c
Abhijeet Bhatt <65093661+Abbhattgithub@users.noreply.github.com>
2024-08-24 05:54:46 +0530

    Random colors on hovering the grid

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 let container = document.getElementById("gridContainer")
 
-for(let cellCount=1; cellCount<=16; cellCount++){
-    let cellDiv = document.createElement("div")
-    cellDiv.setAttribute("class","gridCell")
-    container.appendChild(cellDiv)
+
+makeGrid(16)
+setHover()
+document.getElementById("createSketchpad").addEventListener("click",()=>{
+    let gridCount = prompt("Enter number of columns (max 100)")
+    if(gridCount>0&&gridCount<=100)
+        makeGrid(gridCount)
+    else
+        console.log(gridCount+"exceeds limit");
+})
+
+function makeGrid(gridCount){
+    container.innerHTML=""
+    let cellWidth=100/gridCount
+    for(let cellCount=1; cellCount<=gridCount*gridCount; cellCount++){
+        let cellDiv = document.createElement("div")
+        cellDiv.setAttribute("class","gridCell")
+        cellDiv.style.width = `${cellWidth}%`
+        container.appendChild(cellDiv)
+    }
+    setHover()
+}
+
+function setHover(){
+    let cells = document.getElementsByClassName("gridCell")
+    Array.from(cells).forEach(cell =>{
+        cell.addEventListener("mouseover",()=>{
+            let blueShade = Math.floor(Math.random()*256)
+            let redShade = Math.floor(Math.random()*256)
+            let greenShade = Math.floor(Math.random()*256)
+    
+            cell.style.backgroundColor = `rgb(${redShade},${blueShade},${greenShade})`;
+        })
+        cell.addEventListener("mouseout",()=>{
+            cell.style.backgroundColor = `white`;
+        })
+    })
 }
